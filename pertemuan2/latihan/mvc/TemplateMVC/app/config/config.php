3dc45965e7ace4c85229f8fa783141db337ec18d
renuputra <145459500+renuputra@users.noreply.github.com>
2025-11-13 18:23:25 +0700

    Delete pertemuan2/latihan directory

--- pertemuan2/latihan/mvc/TemplateMVC/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("BASEURL", "http://localhost/prakweb2025_C_233040117/pertemuan2/latihan/mvc/TemplateMVC/public/");
-
-// DATANASE
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'prakweb2025_perteuan2');
